191749c0fc2bf23c56ea17b603543b2c1cf8bba8
Sveta <pupsadon@gmail.com>
2025-08-15 23:27:31 +0300

    correct h-w 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,11 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
   if (totalPrice > customerCredits) {
-    return 'Insufficient funds';
+    return 'Insufficient funds!';
   }
-  return `You ordered ${quantity} droids, you have ${
-    customerCredits - totalPrice
-  } credits left`;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
+
 console.log(makeTransaction(5, 3000, 23000));
 console.log(makeTransaction(3, 1000, 15000));
 console.log(makeTransaction(10, 5000, 8000));
